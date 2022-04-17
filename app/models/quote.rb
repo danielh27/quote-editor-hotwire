4356003c1eb657e16e75155398932f7112b8221e
Daniel Herrera <daniel97hh@gmail.com>
2022-04-17 01:51:46 -0500

    add associations to models

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,8 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   validates :name, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
-
   broadcasts_to ->(_) { 'quotes' }, inserts_by: :prepend
 end
