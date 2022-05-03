48cb4524c599baf1adf18ad316e8890713b79bb7
Daniel Herrera <daniel97hh@gmail.com>
2022-05-03 13:17:38 -0500

    add actions to item dates controller

--- app/controllers/item_dates_controller.rb
+++ app/controllers/item_dates_controller.rb
@@ -1,5 +1,6 @@
 class ItemDatesController < ApplicationController
-  before_action :set_quote, only: %i[new create]
+  before_action :set_quote
+  before_action :set_item_date, only: %i[edit update destroy]
 
   def new
     @item_date = @quote.item_dates.build
@@ -15,6 +16,23 @@ class ItemDatesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item_date.update(item_date_params)
+      redirect_to @quote, notice: "Date was successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item_date.destroy
+
+    redirect_to @quote, notice: "Date was successfully deleted"
+  end
+
   private
 
   def item_date_params
@@ -23,5 +41,9 @@ class ItemDatesController < ApplicationController
 
   def set_quote
     @quote = current_company.quotes.find(params[:quote_id])
+  end
+
+  def set_item_date
+    @item_date = @quote.item_dates.find(params[:id])
   end
 end
